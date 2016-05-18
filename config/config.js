8edc93ff0504e77606eeda81a80e1bef23af6635
sunrain990 <409747494@qq.com>
2016-05-18 10:51:57 +0800

    switch which mongo

--- config/config.js
+++ config/config.js
@@ -1,6 +1,45 @@
 /**
  * Created by kevin on 16/5/17.
  */
-module.exports = {
-    mongodb: "mongodb://localhost/exam"
+
+var os = require('os');
+
+var config = {};
+
+function handleError () {
+
+    var ipv4;
+
+    if (os.networkInterfaces().eth1) {
+        for (var i = 0; i < os.networkInterfaces().eth1.length; i++) {
+            if (os.networkInterfaces().eth1[i].family == 'IPv4') {
+                ipv4 = os.networkInterfaces().eth1[i].address;
+            }
+        }
+        var hostname = os.hostname();
+        //console.log(hostname,ipv4);
+        if (ipv4 == '121.41.41.46') {
+            config.mongodb = 'mongodb://localhost/exam';
+            console.log('informal');
+        } else if (ipv4 == '121.41.123.2') {
+            config.mongodb = 'mongodb://121.41.37.217/exam';
+            console.log('formal');
+        } else if (ipv4 == '120.26.245.233') {
+            config.mongodb = 'mongodb://121.41.41.46/exam';
+            console.log('test',config.mongodb);
+        }
+    } else if (os.networkInterfaces().lo0) {
+        for (var i = 0; i < os.networkInterfaces().lo0.length; i++) {
+            if (os.networkInterfaces().lo0[i].family == 'IPv4') {
+                ipv4 = os.networkInterfaces().lo0[i].address;
+            }
+        }
+        if (ipv4 == '127.0.0.1') {
+            console.log('localhost');
+        }
+    }
 }
+
+handleError();
+
+module.exports = config;
